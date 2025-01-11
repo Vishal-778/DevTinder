d47b8fae9fec3ca37f40f233f6f8867e2bb31e94
Vishal-778 <vishalchaudhary2224@gmail.com>
2025-01-11 17:27:36 +0530

    Routing

--- src/app.js
+++ src/app.js
@@ -2,9 +2,23 @@ const express = require("express");
 
 const app = express();
 
-app.use((req,res) => {
+app.use("/",(req,res) => {
+    res.send("Hello Hello Hello");
+});
+
+app.use("/test",(req,res) => {
     res.send("Hello from the server");
-})
+});
+
+app.use("/xyz",(req,res) => {
+    res.send("Hello ");
+});
+
+
+
+
+
+
 
 app.listen(3000,(req,res) =>{
     console.log("Server is running sucessfully");
